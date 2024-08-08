4a923c018d08f897fa839ec092b94d1d972a8627
RaulFG <raul@artline.es>
2024-08-08 14:03:02 +0200

    add componentes bloque 1 & header

---
+++ resources/views/components/autoevaluacion/bloque1.blade.php
@@ -0,0 +1,8 @@
+<x-autoevaluacion.header title="Datos demográficos">
+    Recuerda que este formulario es una herramienta de reflexión personal elaborado por la Fundación
+    Adecco y que los resultados serán absolutamente anónimos. Únicamente nos servirán para conocer el nivel de
+    compromiso y conocimiento de la Diversidad, Equidad e Inclusión y el nivel de sensibilidad de las empresas con
+    determinados grupos sociales y minorías. De esta manera podremos enfocar mejor las estrategias de liderazgo
+    inclusivo y ético, la formación y el desarrollo de competencias y comportamientos clave para crear entornos
+    empresariales abiertos a la diversidad y más inclusivos.
+</x-autoevaluacion.header>
