d74c6256dd3fdc260a1bb2cf6098fdbee3630896
RaulFG <raul@artline.es>
2024-08-30 09:49:30 +0200

    add paginacion funcionando

--- resources/views/components/autoevaluacion/navegacion.blade.php
+++ resources/views/components/autoevaluacion/navegacion.blade.php
@@ -1,15 +1,15 @@
 <div>
-    <hr class="mt-20">
-    <div class="flex items-center justify-between w-60 mx-auto mt-5">
-        {{-- button rounded --}}
-        <button id="btn-prev" class="btn-rounded disabled">
-            <x-heroicon-m-chevron-left class="w-8 h-8" />
-        </button>
-        <div>
-            <span class="font-bold">Pagina <span>1</span></span> de <span>6</span>
-        </div>
-        <button id="btn-next" class="btn-rounded">
-            <x-heroicon-m-chevron-right class="w-8 h-8" />
-        </button>
+  <hr class="mt-20">
+  <div class="mx-auto mt-5 flex w-60 items-center justify-between">
+    {{-- button rounded --}}
+    <button id="btn-prev" class="btn-rounded disabled">
+      <x-heroicon-m-chevron-left class="h-8 w-8" />
+    </button>
+    <div>
+      <span class="font-bold">Pagina <span class="current-page">1</span></span> de <span class="total-page">6</span>
     </div>
+    <button id="btn-next" class="btn-rounded">
+      <x-heroicon-m-chevron-right class="h-8 w-8" />
+    </button>
+  </div>
 </div>
--- resources/views/livewire/autoeval-component.blade.php
+++ resources/views/livewire/autoeval-component.blade.php
@@ -1,63 +1,157 @@
 <div>
-    {{-- {{ $survey }} --}}
+  {{-- {{ $survey }} --}}
 
-    <div class="home p-10 min-h-screen">
-        <div class="container mx-auto">
-            <header>
-                <img src="img/logo1_n.svg" alt="">
-            </header>
+  <div class="home min-h-screen p-10">
+    <div class="container mx-auto">
+      <header>
+        <img src="img/logo1_n.svg" alt="">
+      </header>
 
-            <div class="intro max-w-[1200px] mx-auto">
+      <div class="intro mx-auto max-w-[1200px]">
 
-                <h1 class="font-lust text-5xl mt-24 text-center ">Reflexiona y evalúa tu liderazgo inclusivo
-                    o
-                    para la inclusión.</h1>
+        <h1 class="mt-24 text-center font-lust text-5xl">Reflexiona y evalúa tu liderazgo inclusivo
+          o
+          para la inclusión.</h1>
 
-                <div class="lg:flex lg:items-start mt-20 px-20">
-                    <div
-                        class="bocadillo bg-white text-2xl p-8  w-[500px] rounded border-2 border-dashed border-verde-adecco relative">
-                        <p>
-                            <span class="">Diversidad</span> es que te inviten a una fiesta. <span
-                                class="text-verde-adecco">Inclusión</span> es que te saquen a bailar, <span
-                                class="text-verde-adecco">equidad</span> que se
-                            acuerden de poner tu canción preferida y <span class="text-verde-adecco">pertenencia</span>
-                            es
-                            bailar como si nadie te estuviera mirando.
-                        </p>
-                    </div>
-                    {{-- foto --}}
-                    <img class="h-[575px] -ml-40 mt-5" src="img/img_chica.webp" alt="">
-                </div>
-
-                {{-- contendido de la autevaluacion --}}
-                <section class="content-eval rounded-3xl bg-white shadow-xl w-full min-h-96 p-16">
-                    <x-autoevaluacion.bloque1 />
-                    <x-autoevaluacion.bloque2 class="mt-20" />
-                    <x-autoevaluacion.bloque3 class="mt-20" />
-                    <x-autoevaluacion.bloque4 class="mt-20" />
-                    <x-autoevaluacion.bloque5 class="mt-20" />
-                    <x-autoevaluacion.bloque6 class="mt-20" />
-                    <x-autoevaluacion.navegacion />
-                </section>
-
-            </div>
+        <div class="mt-20 px-20 lg:flex lg:items-start">
+          <div class="bocadillo relative w-[500px] rounded border-2 border-dashed border-verde-adecco bg-white p-8 text-2xl">
+            <p>
+              <span class="">Diversidad</span> es que te inviten a una fiesta. <span class="text-verde-adecco">Inclusión</span> es que te saquen a bailar, <span
+                class="text-verde-adecco">equidad</span> que se
+              acuerden de poner tu canción preferida y <span class="text-verde-adecco">pertenencia</span>
+              es
+              bailar como si nadie te estuviera mirando.
+            </p>
+          </div>
+          {{-- foto --}}
+          <img class="-ml-40 mt-5 h-[530px]" src="img/img_chica.webp" alt="">
         </div>
+
+        {{-- contendido de la autevaluacion --}}
+        <section class="content-eval min-h-96 w-full rounded-3xl bg-white p-16 shadow-xl">
+          <x-autoevaluacion.bloque1 id="block-1" />
+          <x-autoevaluacion.bloque2 id="block-2" class="hidden" />
+          <x-autoevaluacion.bloque3 id="block-3" class="hidden" />
+          <x-autoevaluacion.bloque4 id="block-4" class="hidden" />
+          <x-autoevaluacion.bloque5 id="block-5" class="hidden" />
+          <x-autoevaluacion.bloque6 id="block-6" class="hidden" />
+          <x-autoevaluacion.navegacion />
+        </section>
+
+      </div>
     </div>
+  </div>
 
 
-    @script
-        <script>
-            document.addEventListener('livewire:initialized', () => {
-                // Runs immediately after Livewire has finished initializing
-                // on the page...
-                console.log("livewire:initialized'");
-                let survey = @json($survey);
-                const {
-                    client,
-                    question
-                } = survey;
-                console.log(question);
+  @script
+    <script>
+      document.addEventListener('livewire:initialized', () => {
+
+        // Runs immediately after Livewire has finished initializing
+        // on the page...
+        console.log("livewire:initialized'");
+        let survey = @json($survey);
+        const {
+          client,
+          question
+        } = survey;
+        console.log(question);
+
+
+        //paginacion
+        const totalPage = 6;
+        let currentPage = 1;
+        const btnNext = $('#btn-next');
+        const btnPrev = $('#btn-prev');
+
+
+
+        initSurvey();
+
+        irPage(4)
+
+
+        function initSurvey() {
+          // Ocultar todos los article menos el id=1
+          // $$('article').forEach(question => {
+          //   question.classList.add('hidden');
+          // })
+          // Mostrar el primero
+          // $('#block-1').classList.remove('hidden');
+
+          // add ids in .question and ansewr
+          $$('.question').forEach((question, index) => {
+            question.setAttribute('id', `question-${index + 1}`);
+          })
+          $$('.answer').forEach((answer, index) => {
+            answer.setAttribute('id', `answer-${index + 1}`);
+          })
+        }
+
+        // para debug
+        function irPage(page) {
+          currentPage = page;
+          changePage();
+        }
+
+        // SELECT number
+
+        $$('.number').forEach(answer => {
+          answer.addEventListener('click', () => {
+            // buscamos el id la question padre de este elemento
+            const question = answer.closest('.question');
+            const questionId = question.id;
+            // const questionNumber = questionId.split('-')[1];
+            // marcamos con la clase .selected
+            $$('.number').forEach(number => {
+
+
             })
-        </script>
-    @endscript
+            // answer.classList.add('selected');
+
+
+          });
+        });
+
+        //   PAGINACION
+        btnNext.addEventListener('click', () => {
+          if (currentPage < totalPage) {
+            currentPage++;
+            changePage();
+          }
+        })
+        btnPrev.addEventListener('click', () => {
+          if (currentPage > 1) {
+            currentPage--;
+            changePage();
+          }
+        })
+        // FUNCTIONS
+        function changePage() {
+          $$('article').forEach(question => {
+            question.classList.add('hidden');
+          })
+          $(`#block-${currentPage}`).classList.remove('hidden');
+
+          btnNext.classList.remove('disabled');
+          btnPrev.classList.remove('disabled');
+          if (currentPage === 1) btnPrev.classList.add('disabled');
+          if (currentPage === totalPage) btnNext.classList.add('disabled');
+
+          // update current page in html
+          $('.current-page').textContent = currentPage;
+          $('.total-page').textContent = totalPage;
+
+        }
+
+        function $(selector) {
+          return document.querySelector(selector);
+        }
+
+        function $$(selector) {
+          return document.querySelectorAll(selector);
+        }
+      })
+    </script>
+  @endscript
 </div>
