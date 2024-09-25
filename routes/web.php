54f4f97959cf7540c2b6037b96184157a9eaeafd
RaulFG <raul@artline.es>
2024-09-25 08:59:29 +0200

    disable route raiz

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', Welcome::class);
+// Route::get('/', Welcome::class);
 
 // Route::get('/', function () {
 //     return view('prueba');
