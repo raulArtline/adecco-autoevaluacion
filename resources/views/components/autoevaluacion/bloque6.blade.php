e38d7c7b5efcaefb3b11807e6e0c8066d19b20b9
RaulFG <raul@artline.es>
2024-09-26 13:47:02 +0200

    changes block-6

--- resources/views/components/autoevaluacion/bloque6.blade.php
+++ resources/views/components/autoevaluacion/bloque6.blade.php
@@ -1,16 +1,18 @@
 <article {{ $attributes }}>
   <x-autoevaluacion.header title="Sensibilidad">
-    ¿Tengo prejuicios o estereotipos sobre determinados grupos sociales? Hay grupos sociales que experimentan en el
-    entorno laboral situaciones de discriminación, rechazo e incluso violencia. La discriminación puede ser
-    inconsciente fruto de sesgos cognitivos aprendidos y fijados en nuestro subconsciente, conscientes fruto de
-    creencias limitantes o también fruto de la falta de contacto y desconocimiento. Estas creencias pueden llevarnos
-    a tratar a determinadas personas de manera injusta o discriminatoria. A continuación reflexiona sobre el nivel
-    de sensibilidad o prejuicio que tienes respecto a los siguientes grupos sociales.
+    ¿Tengo prejuicios o estereotipos sobre determinados grupos sociales? Hay grupos sociales que experimentan en el entorno laboral situaciones de discriminación, rechazo e incluso
+    violencia. La discriminación puede ser: <strong>inconsciente</strong>, fruto de sesgos cognitivos aprendidos y fijados en nuestro subconsciente; <strong>conscientes</strong>,
+    fruto de creencias limitantes o
+    también fruto de la falta de contacto y desconocimiento. Estas creencias pueden llevarnos a tratar a determinadas personas de manera injusta o discriminatoria. A continuación,
+    reflexiona sobre el nivel de sensibilidad o prejuicio que tienes respecto a los siguientes grupos sociales.
+
   </x-autoevaluacion.header>
+
+
 
   <div class="question text-center">
     <p class="mt-3 text-left">(*) Obligatorio.</p>
-    <h4 class="mt-28 text-2xl text-verde-adecco">
+    <h4 class="mt-24 text-2xl text-verde-adecco">
       <span class="p-number">28. </span> Mi <strong class="underline">nivel de sensibilidad</strong> con las
       <strong>personas con discapacidad intelectual</strong> es...(*)
     </h4>
@@ -19,20 +21,11 @@
 
     <x-autoevaluacion.custom-range pretext="Ninguna sensibilidad" postext="Mucha sensibilidad" class="answer flex flex-wrap justify-center md:justify-between gap-2" />
   </div>
-  <!--  -->
-  <div class="question text-center">
-    <h4 class="mt-28 text-2xl text-verde-adecco">
-      <span class="p-number">29. </span> Mi nivel de <strong class="underline">contacto</strong> con las personas
-      con <strong>discapacidad intelectual</strong> es...(*)
-    </h4>
-    <h4 class="font-semibold">(Siendo 0= Ningún contacto | 5= Algo de contacto | 10= Mucho contacto)</h4>
 
-    <x-autoevaluacion.custom-range pretext="Ningún contacto" postext="Mucho contacto" class="answer flex flex-wrap justify-center md:justify-between gap-2" />
-  </div>
   <!--  -->
   <div class="question text-center">
-    <h4 class="mt-28 text-2xl text-verde-adecco">
-      <span class="p-number">30. </span> Mi <strong class="underline">nivel de sensibilidad</strong> con las
+    <h4 class="mt-24 text-2xl text-verde-adecco">
+      <span class="p-number">29. </span> Mi <strong class="underline">nivel de sensibilidad</strong> con las
       personas con problemas de <strong>salud mental</strong> es...(*)
     </h4>
     <h4 class="font-semibold">(Siendo 0= Ninguna sensibilidad | 5= Algo de sensibilidad | 10= Mucha sensibilidad)
@@ -40,20 +33,10 @@
 
     <x-autoevaluacion.custom-range pretext="Ninguna sensibilidad" postext="Mucha sensibilidad" class="answer flex flex-wrap justify-center md:justify-between gap-2" />
   </div>
-  <!--  -->
-  <div class="question text-center">
-    <h4 class="mt-28 text-2xl text-verde-adecco">
-      <span class="p-number">31. </span> Mi <strong class="underline">contacto</strong> con las personas con
-      problemas de <strong>salud mental</strong> es...(*)
-    </h4>
-    <h4 class="font-semibold">(Siendo 0= Ningún contacto | 5= Algo de contacto | 10= Mucho contacto)</h4>
 
-    <x-autoevaluacion.custom-range pretext="Ningún contacto" postext="Mucho contacto" class="answer flex flex-wrap justify-center md:justify-between gap-2" />
-  </div>
-  <!--  -->
   <div class="question text-center">
-    <h4 class="mt-28 text-2xl text-verde-adecco">
-      <span class="p-number">32. </span> Mi <strong class="underline">nivel de sensibilidad</strong> con las
+    <h4 class="mt-24 text-2xl text-verde-adecco">
+      <span class="p-number">30. </span> Mi <strong class="underline">nivel de sensibilidad</strong> con las
       personas con <strong>discapacidad física</strong> (movilidad reducida, amputaciones, parálisis...) es...(*)
     </h4>
     <h4 class="font-semibold">(Siendo 0= Ninguna sensibilidad | 5= Algo de sensibilidad | 10= Mucha sensibilidad)
@@ -61,20 +44,11 @@
 
     <x-autoevaluacion.custom-range pretext="Ninguna sensibilidad" postext="Mucha sensibilidad" class="answer flex flex-wrap justify-center md:justify-between gap-2" />
   </div>
-  <!--  -->
-  <div class="question text-center">
-    <h4 class="mt-28 text-2xl text-verde-adecco">
-      <span class="p-number">33. </span> Mi <strong class="underline">contacto</strong> con las personas con
-      <strong>discapacidad física</strong> (movilidad reducida, amputaciones, parálisis...) es...(*)
-    </h4>
-    <h4 class="font-semibold">(Siendo 0= Ningún contacto | 5= Algo de contacto | 10= Mucho contacto)</h4>
 
-    <x-autoevaluacion.custom-range pretext="Ningún contacto" postext="Mucho contacto" class="answer flex flex-wrap justify-center md:justify-between gap-2" />
-  </div>
   <!--  -->
   <div class="question text-center">
-    <h4 class="mt-28 text-2xl text-verde-adecco">
-      <span class="p-number">34. </span> Mi <strong class="underline">nivel de sensibilidad</strong> con las
+    <h4 class="mt-24 text-2xl text-verde-adecco">
+      <span class="p-number">31. </span> Mi <strong class="underline">nivel de sensibilidad</strong> con las
       personas con <strong>discapacidad visual</strong> (personas ciegas) es...(*)
     </h4>
     <h4 class="font-semibold">(Siendo 0= Ninguna sensibilidad | 5= Algo de sensibilidad | 10= Mucha sensibilidad)
@@ -83,36 +57,107 @@
     <x-autoevaluacion.custom-range pretext="Ninguna sensibilidad" postext="Mucha sensibilidad" class="answer flex flex-wrap justify-center md:justify-between gap-2" />
   </div>
   <!--  -->
-  <div class="question text-center">
-    <h4 class="mt-28 text-2xl text-verde-adecco">
-      <span class="p-number">35. </span> Mi <strong class="underline">contacto</strong> con las personas con
-      <strong>discapacidad visual</strong> (personas ciegas) es...(*)
-    </h4>
-    <h4 class="font-semibold">(Siendo 0= Ningún contacto | 5= Algo de contacto | 10= Mucho contacto)</h4>
 
-    <x-autoevaluacion.custom-range pretext="Ningún contacto" postext="Mucho contacto" class="answer flex flex-wrap justify-center md:justify-between gap-2" />
-  </div>
-  <!--  -->
   <div class="question text-center">
-    <h4 class="mt-28 text-2xl text-verde-adecco">
-      <span class="p-number">36. </span> Mi <strong class="underline">nivel de sensibilidad</strong> con las
+    <h4 class="mt-24 text-2xl text-verde-adecco">
+      <span class="p-number">32. </span> Mi <strong class="underline">nivel de sensibilidad</strong> con las
       personas con <strong>discapacidad auditiva</strong> (personas sordas) es...(*)
     </h4>
     <h4 class="font-semibold">(Siendo 0= Ninguna sensibilidad | 5= Algo de sensibilidad | 10= Mucha sensibilidad)
     </h4>
-
     <x-autoevaluacion.custom-range pretext="Ninguna sensibilidad" postext="Mucha sensibilidad" class="answer flex flex-wrap justify-center md:justify-between gap-2" />
   </div>
   <!--  -->
   <div class="question text-center">
-    <h4 class="mt-28 text-2xl text-verde-adecco">
-      <span class="p-number">37. </span> Mi <strong class="underline">contacto</strong> con las personas con
-      <strong>discapacidad auditiva</strong> (personas sordas) es...(*)
+    <h4 class="mt-24 text-2xl text-verde-adecco">
+      <span class="p-number">33. <strong class="underline">Mi nivel de sensibilidad </strong>con la defensa e igualdad de oportunidades para las mujeres es...
     </h4>
-    <h4 class="font-semibold">(Siendo 0= Ningún contacto | 5= Algo de contacto | 10= Mucho contacto)</h4>
-
-    <x-autoevaluacion.custom-range pretext="Ningún contacto" postext="Mucho contacto" class="answer flex flex-wrap justify-center md:justify-between gap-2" />
+    <h4 class="font-semibold">(Siendo 0= Ninguna sensibilidad | 5= Algo de sensibilidad | 10= Mucha sensibilidad)
+    </h4>
+    <x-autoevaluacion.custom-range pretext="Ninguna sensibilidad" postext="Mucha sensibilidad" class="answer flex flex-wrap justify-center md:justify-between gap-2" />
   </div>
   <!--  -->
+  <div class="question text-center">
+    <h4 class="mt-24 text-2xl text-verde-adecco">
+      <span class="p-number">34. <strong class="underline">Mi nivel de sensibilidad </strong>con la situación y realidad de las mujeres víctimas de violencia de género es...
+    </h4>
+    <h4 class="font-semibold">(Siendo 0= Ninguna sensibilidad | 5= Algo de sensibilidad | 10= Mucha sensibilidad)
+    </h4>
+    <x-autoevaluacion.custom-range pretext="Ninguna sensibilidad" postext="Mucha sensibilidad" class="answer flex flex-wrap justify-center md:justify-between gap-2" />
+  </div>
+  <!--  -->
+  <div class="question text-center">
+    <h4 class="mt-24 text-2xl text-verde-adecco">
+      <span class="p-number">35. <strong class="underline">Mi nivel de sensibilidad </strong>con las personas negras o afrodescendientes es...
+    </h4>
+    <h4 class="font-semibold">(Siendo 0= Ninguna sensibilidad | 5= Algo de sensibilidad | 10= Mucha sensibilidad)
+    </h4>
+    <x-autoevaluacion.custom-range pretext="Ninguna sensibilidad" postext="Mucha sensibilidad" class="answer flex flex-wrap justify-center md:justify-between gap-2" />
+  </div>
+  <!--  -->
+  <div class="question text-center">
+    <h4 class="mt-24 text-2xl text-verde-adecco">
+      <span class="p-number">36. <strong class="underline">Mi nivel de sensibilidad </strong>con las personas de origen latino es...
+    </h4>
+    <h4 class="font-semibold">(Siendo 0= Ninguna sensibilidad | 5= Algo de sensibilidad | 10= Mucha sensibilidad)
+    </h4>
+    <x-autoevaluacion.custom-range pretext="Ninguna sensibilidad" postext="Mucha sensibilidad" class="answer flex flex-wrap justify-center md:justify-between gap-2" />
+  </div>
+  <!--  -->
+  <div class="question text-center">
+    <h4 class="mt-24 text-2xl text-verde-adecco">
+      <span class="p-number">37. <strong class="underline">Mi nivel de sensibilidad </strong>con las personas de origen árabe es...
+    </h4>
+    <h4 class="font-semibold">(Siendo 0= Ninguna sensibilidad | 5= Algo de sensibilidad | 10= Mucha sensibilidad)
+    </h4>
+    <x-autoevaluacion.custom-range pretext="Ninguna sensibilidad" postext="Mucha sensibilidad" class="answer flex flex-wrap justify-center md:justify-between gap-2" />
+  </div>
+  <!--  -->
+  <div class="question text-center">
+    <h4 class="mt-24 text-2xl text-verde-adecco">
+      <span class="p-number">38. <strong class="underline">Mi nivel de sensibilidad </strong>con las personas de origen asiático es...
+    </h4>
+    <h4 class="font-semibold">(Siendo 0= Ninguna sensibilidad | 5= Algo de sensibilidad | 10= Mucha sensibilidad)
+    </h4>
+    <x-autoevaluacion.custom-range pretext="Ninguna sensibilidad" postext="Mucha sensibilidad" class="answer flex flex-wrap justify-center md:justify-between gap-2" />
+  </div>
+  <!--  -->
+  <div class="question text-center">
+    <h4 class="mt-24 text-2xl text-verde-adecco">
+      <span class="p-number">39. <strong class="underline">Mi nivel de sensibilidad </strong>con las personas pertenecientes al colectivo LGTBI+ es...
+    </h4>
+    <h4 class="font-semibold">(Siendo 0= Ninguna sensibilidad | 5= Algo de sensibilidad | 10= Mucha sensibilidad)
+    </h4>
+    <x-autoevaluacion.custom-range pretext="Ninguna sensibilidad" postext="Mucha sensibilidad" class="answer flex flex-wrap justify-center md:justify-between gap-2" />
+  </div>
+  <!--  -->
+  <div class="question text-center">
+    <h4 class="mt-24 text-2xl text-verde-adecco">
+      <span class="p-number">40. <strong class="underline">Mi nivel de sensibilidad </strong>con las personas mayores de 65 años es...
+    </h4>
+    <h4 class="font-semibold">(Siendo 0= Ninguna sensibilidad | 5= Algo de sensibilidad | 10= Mucha sensibilidad)
+    </h4>
+    <x-autoevaluacion.custom-range pretext="Ninguna sensibilidad" postext="Mucha sensibilidad" class="answer flex flex-wrap justify-center md:justify-between gap-2" />
+  </div>
+  <!--  -->
+  <div class="question text-center">
+    <h4 class="mt-24 text-2xl text-verde-adecco">
+      <span class="p-number">41. <strong class="underline">Mi nivel de sensibilidad</strong> con las personas senior (personas entre 45 y 65 años) en el entorno laboral es...
+    </h4>
+    <h4 class="font-semibold">(Siendo 0= Ninguna sensibilidad | 5= Algo de sensibilidad | 10= Mucha sensibilidad)
+    </h4>
+    <x-autoevaluacion.custom-range pretext="Ninguna sensibilidad" postext="Mucha sensibilidad" class="answer flex flex-wrap justify-center md:justify-between gap-2" />
+  </div>
+  <!--  -->
+  <div class="question text-center">
+    <h4 class="mt-24 text-2xl text-verde-adecco">
+      <span class="p-number">42. <strong class="underline">Mi nivel de sensibilidad </strong>con las personas de las nuevas generaciones (menores de 25 años) es...
+    </h4>
+    <h4 class="font-semibold">(Siendo 0= Ninguna sensibilidad | 5= Algo de sensibilidad | 10= Mucha sensibilidad)
+    </h4>
+    <x-autoevaluacion.custom-range pretext="Ninguna sensibilidad" postext="Mucha sensibilidad" class="answer flex flex-wrap justify-center md:justify-between gap-2" />
+  </div>
+  <!--  -->
+
 
 </article>
