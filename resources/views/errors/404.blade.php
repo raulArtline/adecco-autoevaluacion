afa4749fab0e9c59411649b1aab194dda1709f83
RaulFG <raul@artline.es>
2024-09-27 10:51:08 +0200

    add custom 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+<x-layouts.app>
+
+  <section>
+    <div class="container min-h-screen px-6 py-12 mx-auto flex items-center justify-center">
+      <div class="text-center">
+        <img width="700" src="/img/illustration.svg" alt="">
+        <p class="mt-10 text-3xl font-bold text-verde-adecco">404 error</p>
+        <h1 class="mt-3 text-5xl  font-lust text-gray-800">Página no encontrada</h1>
+        <p class="mt-4 text-2xl">Lo sentimos, la página que estás buscando no existe.</p>
+      </div>
+    </div>
+
+  </section>
+
+</x-layouts.app>
